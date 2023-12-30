735f2f3322a301dfc70367418fb8c90cd8e91933
Shivam Kumar Gaurav <shivamshroff111@gmail.com>
2023-12-30 14:24:40 +0530

    middlewares added for authentication

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,13 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
+//cookie-parser - what is this and why we need this ?
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 app.use(express.json());
+
 //connecting to database
 require("./config/database").connect();
 
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User");
+
 const {login, signup} = require("../Controllers/Auth");
+// const {auth, isStudent,isAdmin} = require("../middlewares/middleAuth");
+const {auth} = require("../middlewares/middleAuth");
 
 router.post("/login", login);
 router.post("/signup", signup);
 
-// const User = require("../models/User");
+//testing protected routes for single middleware
+router.get("/test", auth, (req,res) =>{
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for TESTS',
+    });
+});
+
+//Protected Route
+// router.get("/student", auth, isStudent, (req,res) => {
+//     res.json({
+//         success:true,
+//         message:'Welcome to the Protected route for Students',
+//     });
+// } );
+
+// router.get("/admin", auth, isAdmin, (req,res) => {
+//     res.json({
+//         success:true,
+//         message:'Welcome to the Protected route for Admin',
+//     });
+// });
+
+
+
+
 module.exports = router;
